faf10bb100391fdaba1826a2e8233505653cc616
DAiKi <agkgmyat2001@gmail.com>
2026-05-11 23:28:52 +0700

    Final

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      'title': ['Barlow Condensed'],
+      'body': ['Montserrat']
+    }
+  },
+  plugins: [],
+}
+
